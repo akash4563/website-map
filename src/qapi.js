f25eb3a626c89ffe90e12d39019af04e4ab3d236
Akash Das <66129630+akash4563@users.noreply.github.com>
2021-03-02 18:49:12 +0530

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"603e370c752fdd001f93591a",
+				"603e370c752fdd001f93591c"
+			]
+		},
+		"603e370c752fdd001f93591a": {
+			"id": "603e370c752fdd001f93591a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"603e370c752fdd001f93591c": {
+			"id": "603e370c752fdd001f93591c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
